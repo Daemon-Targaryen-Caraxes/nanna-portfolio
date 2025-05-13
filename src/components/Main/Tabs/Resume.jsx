c2b17cea378a8030052e7d3eb9b97e48e2e2c59d
Hemeswar reddy <157202138+Daemon-Targaryen-Caraxes@users.noreply.github.com>
2025-05-13 19:08:30 +0530

    completed

--- src/components/Main/Tabs/Resume.jsx
+++ src/components/Main/Tabs/Resume.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+// import cv from '../../../assets/documents/hemsCV.pdf'
 
 const Resume = () => {
   return (
-    <div>Resume</div>
+    <div id='resume'>
+      {/* <a href={cv} download={}></a> */}
+    </div>
   )
 }
 
