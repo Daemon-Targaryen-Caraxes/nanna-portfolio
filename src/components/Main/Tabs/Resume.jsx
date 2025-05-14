72b7e0618f3a7d14b154b7b8fd24bae3f4869a15
Hemeswar reddy <157202138+Daemon-Targaryen-Caraxes@users.noreply.github.com>
2025-05-14 16:29:40 +0530

    completed resume tab

--- src/components/Main/Tabs/Resume.jsx
+++ src/components/Main/Tabs/Resume.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-// import cv from '../../../assets/documents/hemsCV.pdf'
+import { Worker, Viewer } from '@react-pdf-viewer/core';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
 const Resume = () => {
+
   return (
     <div id='resume'>
-      {/* <a href={cv} download={}></a> */}
+      <a href="/hemsCV2.pdf" download='nanna-cv.pdf'>Download here</a>
+      <div id='resume-pdf'>
+        <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
+          <Viewer
+            fileUrl="/hemsCV2.pdf"
+          />
+        </Worker>
+      </div>
     </div>
   )
 }
